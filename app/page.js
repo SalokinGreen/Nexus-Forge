74d2d0ee101aa160d558964c6621831b45fa83f7
SalokinGreen <salokingreen@gmail.com>
2023-05-04 22:48:19 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -42,58 +42,51 @@ export default function Home() {
     return <div>Loading...</div>;
   }
 
-  try {
-    return (
-      <div className={styles.container}>
-        <Navbar />
-        <Head>
-          <title>World-Building App</title>
-          <meta
-            name="description"
-            content="Welcome to the World-Building App"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <Head>
+        <title>World-Building App</title>
+        <meta name="description" content="Welcome to the World-Building App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>Welcome to the Nexus-Forge</h1>
-          <p className={styles.description}>
-            Explore, create, and share your own worlds and stories!
-          </p>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to the Nexus-Forge</h1>
+        <p className={styles.description}>
+          Explore, create, and share your own worlds and stories!
+        </p>
+        {!loading && <>
+        <section className={styles.articlesSection}>
+          <h2>Favorite Articles</h2>
+          <div className={styles.articles}>
+            {favoriteArticles.map((article) => (
+              <ArticlePreview
+                key={article.id}
+                id={article.id}
+                title={article.title}
+                content={article.content}
+                images={article.images}
+              />
+            ))}
+          </div>
+        </section>
 
-          <section className={styles.articlesSection}>
-            <h2>Favorite Articles</h2>
-            <div className={styles.articles}>
-              {favoriteArticles.map((article) => (
-                <ArticlePreview
-                  key={article.id}
-                  id={article.id}
-                  title={article.title}
-                  content={article.content}
-                  images={article.images}
-                />
-              ))}
-            </div>
-          </section>
-
-          <section className={styles.articlesSection}>
-            <h2>New Articles</h2>
-            <div className={styles.articles}>
-              {newArticles.map((article) => (
-                <ArticlePreview
-                  key={article.id}
-                  id={article.id}
-                  title={article.title}
-                  content={article.content}
-                  images={article.images}
-                />
-              ))}
-            </div>
-          </section>
-        </main>
-      </div>
-    );
-  } catch (error) {
-    console.log(error);
-  }
+        <section className={styles.articlesSection}>
+          <h2>New Articles</h2>
+          <div className={styles.articles}>
+            {newArticles.map((article) => (
+              <ArticlePreview
+                key={article.id}
+                id={article.id}
+                title={article.title}
+                content={article.content}
+                images={article.images}
+              />
+            ))}
+          </div>
+        </section></>}
+      </main>
+    </div>
+  );
 }
