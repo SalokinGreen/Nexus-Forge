263052c48036def2971e0618b880fc5c450fb388
SalokinGreen <salokingreen@gmail.com>
2023-05-04 22:48:33 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -56,36 +56,39 @@ export default function Home() {
         <p className={styles.description}>
           Explore, create, and share your own worlds and stories!
         </p>
-        {!loading && <>
-        <section className={styles.articlesSection}>
-          <h2>Favorite Articles</h2>
-          <div className={styles.articles}>
-            {favoriteArticles.map((article) => (
-              <ArticlePreview
-                key={article.id}
-                id={article.id}
-                title={article.title}
-                content={article.content}
-                images={article.images}
-              />
-            ))}
-          </div>
-        </section>
+        {!loading && (
+          <>
+            <section className={styles.articlesSection}>
+              <h2>Favorite Articles</h2>
+              <div className={styles.articles}>
+                {favoriteArticles.map((article) => (
+                  <ArticlePreview
+                    key={article.id}
+                    id={article.id}
+                    title={article.title}
+                    content={article.content}
+                    images={article.images}
+                  />
+                ))}
+              </div>
+            </section>
 
-        <section className={styles.articlesSection}>
-          <h2>New Articles</h2>
-          <div className={styles.articles}>
-            {newArticles.map((article) => (
-              <ArticlePreview
-                key={article.id}
-                id={article.id}
-                title={article.title}
-                content={article.content}
-                images={article.images}
-              />
-            ))}
-          </div>
-        </section></>}
+            <section className={styles.articlesSection}>
+              <h2>New Articles</h2>
+              <div className={styles.articles}>
+                {newArticles.map((article) => (
+                  <ArticlePreview
+                    key={article.id}
+                    id={article.id}
+                    title={article.title}
+                    content={article.content}
+                    images={article.images}
+                  />
+                ))}
+              </div>
+            </section>
+          </>
+        )}
       </main>
     </div>
   );
